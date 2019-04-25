c9fe55f1c586ebeb2163511ab570ee46e297cafb
martinez <16816720606@qq.com>
2019-04-25 16:40:30 +0800

    "Second Commit"

--- Read_data_from_sql_table.py
+++ Read_data_from_sql_table.py
@@ -1,9 +1,14 @@
 import pandas as pd 
 import pymysql
-
-db=pymysql.connect("localhost","root","","cheka")
+# Database details
+host_name="localhost"
+username="username"
+password="pass"
+db_name="db_name"
+# Creating a connection to the database
+db=pymysql.connect(host_name,username,password,db_name)
 cursor =db.cursor()
-
+# Quaery to select data from db table
 query= "SELECT * FROM tp_menu WHERE is_dev=2"
 list1=[]
 try:
@@ -13,13 +18,13 @@ try:
 	list1=list(data)
 except:
 	db.rollback()
-# for row in data.split("\n"):
-# 	list.append(row.split(","))
+
 df= pd.DataFrame(list1)
 print(df)
 drop_query ="DELETE FROM tp_users where id=3"
 cursor.execute(drop_query)
 db.commit()
+# query to insert data to db table
 query_insert ="INSERT INTO tp_menu VALUES((48,'Programming', 'gear', 0, 100, 'New_Menu/index', 0, '', '', 2))"
 try:
 	cursor.execute(query_insert)
